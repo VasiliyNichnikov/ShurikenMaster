131323404432701cde7ab2e869db72b204d6e233
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-03-25 16:03:57 +0300

    Написан код, который перемещает выбранный объект по кривой

---
+++ Assets/Scripts/MovementAlongSpline/Engine.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace MovementAlongSpline
+{
+    public class Engine : MonoBehaviour
+    {
+        [SerializeField, Range(1, 500)] private float _speedMovement;
+        [SerializeField, Range(1, 100)] private float _speedRotation;
+        [SerializeField, Range(0, 1)] private float _spacing;
+        [SerializeField] private Spline.Spline _spline;
+        [SerializeField] private Transform _selectedObject;
+
+        private IRoute _route;
+        private IMovement _movement;
+        
+        private void Start()
+        {
+            CreateRoute();
+            CreateMovement();
+            ToRun();
+        }
+
+        private void CreateRoute()
+        {
+            _route = new Route(_spline);
+            _route.Spacing = _spacing;
+            _route.Build();
+        }
+
+        private void CreateMovement()
+        {
+            _movement = new Movement(_selectedObject, _route.Points);
+            _movement.SpeedMovement = _speedMovement;
+            _movement.SpeedRotation = _speedRotation;
+        }
+
+        private void ToRun()
+        {
+            StartCoroutine(_movement.Drive());
+        }
+        
+    }
+}
---
+++ Assets/Scripts/MovementAlongSpline/IMovement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+namespace MovementAlongSpline
+{
+    public interface IMovement
+    {
+        public float SpeedMovement { get; set; }
+        public float SpeedRotation { get; set; }
+        public IEnumerator Drive();
+    }
+}
