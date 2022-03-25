f9d77b96702adcb78a6bb1a13b88fbc8e5f798a1
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-03-25 20:28:55 +0300

    Добавлены параметры

---
+++ Assets/Scripts/Parameters/Player/PlayerParameters.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Parameters.Player
+{
+    [CreateAssetMenu(fileName = "Player", menuName = "Parameters/Player", order = 0)]
+    public class PlayerParameters : ScriptableObject
+    {
+        public MotionParameter DefaultMotion => _defaultMotion;
+        public MotionParameter SlowDownMotion => _slowDownMotion;
+
+        [SerializeField, Header("Параметр движения при обычной скорости")]
+        private MotionParameter _defaultMotion;
+
+        [SerializeField, Header("Параметр движения при замедлении")]
+        private MotionParameter _slowDownMotion;
+    }
+}
