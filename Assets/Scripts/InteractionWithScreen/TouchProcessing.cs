2a12e09be719f8f5653b0de1dd9d4765304e8b72
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-03-26 20:38:04 +0300

    Добавлена сборка для работы с экраном

---
+++ Assets/Scripts/InteractionWithScreen/TouchProcessing.cs
@@ -0,0 +1,29 @@
+﻿using DrawSlicing;
+using UnityEngine;
+
+namespace InteractionWithScreen
+{
+    public class TouchProcessing : MonoBehaviour
+    {
+        [SerializeField] private DrawTrail _clickTrail;
+
+        private void Update()
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            if (Input.GetMouseButtonDown(0))
+            {
+                _clickTrail.ClickDown(mousePosition);
+            }
+
+            else if (Input.GetMouseButton(0))
+            {
+                _clickTrail.ClickDrag(mousePosition);
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                _clickTrail.ClickUp(mousePosition);
+            }
+        }
+    }
+}
