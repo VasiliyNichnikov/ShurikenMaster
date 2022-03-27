c719eb121f69758beeaa00c15b3e29b8390ffbc3
Daniil <DaniilDany32@gmail.com>
2022-03-27 20:16:52 +0300

    Новый тестовый уровень

--- Assets/Resources/Prefabs/HelicopterRotation.cs
+++ Assets/Resources/Prefabs/HelicopterRotation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShurikenRotation : MonoBehaviour
+public class HelicopterRotation : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class ShurikenRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(Time.deltaTime * 50, Time.deltaTime * 300, 0));
+        
     }
 }
