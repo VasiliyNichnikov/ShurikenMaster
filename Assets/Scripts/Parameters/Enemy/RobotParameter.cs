3ed3c2cc6cc184d488c55c12257ecd8112616cb2
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-03-27 20:42:49 +0300

    Добавлены параметры Enemy

---
+++ Assets/Scripts/Parameters/Enemy/RobotParameter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Parameters.Enemy
+{
+    [CreateAssetMenu(fileName = "Robot", menuName = "Parameters/Robot", order = 0)]
+    public class RobotParameter : ScriptableObject
+    {
+        public float MinDelayBetweenShooting => _minDelayBetweenShooting;
+        public float MaxDelayBetweenShooting => _maxDelayBetweenShooting;
+        public float DelayBeforeFocusing => _delayBeforeFocusing;
+        public float SpeedRotationHead => _speedRotatationHead;
+        public float SpeedBullet => _speedBullet;
+        public GameObject Bullet => _bullet;
+        public float MinAngle => _minAngle;
+
+        [SerializeField, Header("Задержка перед тем как начать двигать головную часть в сторону игрока"), Range(0, 10)]
+        private float _delayBeforeFocusing;
+
+        [SerializeField, Header("Скорость движение вращения головной части"), Range(0, 50)]
+        private float _speedRotatationHead;
+
+        [SerializeField, Header("Префаб патрона")]
+        private GameObject _bullet;
+
+        [SerializeField, Header("Скорость патрона"), Range(0, 10)]
+        private float _speedBullet;
+
+        [SerializeField, Header("Минимальный угол пока будет происходить движение головной части"), Range(0, 180)]
+        private float _minAngle;
+
+        [SerializeField, Header("Минимальная задержка между стрельбой"), Range(0, 10)]
+        private float _minDelayBetweenShooting;
+
+        [SerializeField, Header("Максимальная задержкам между стрельбой"), Range(0, 10)]
+        private float _maxDelayBetweenShooting;
+    }
+}
---
+++ Assets/Scripts/Parameters/Enemy/StickmanParameter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Parameters.Enemy
+{
+    [CreateAssetMenu(fileName = "StickmanParameter", menuName = "Parameters/StickmanParameter", order = 0)]
+    public class StickmanParameter : ScriptableObject
+    {
+        public float Speed => _speed;
+        public float MinDistanceToPoint => _minDistanceToPoint;
+        public float NumberIdleAnimator => _numberIdleAnimator;
+        public float NumberWalkAnimator => _numberWalkAnimator;
+        public float NumberAttackAnimator => _numberAttackAnimator;
+        public string NameAnimatorSwitcher => _nameAnimatorSwitcher;
+
+        [SerializeField, Header("Скорость движения к точке"), Range(0.001f, 5f)]
+        private float _speed;
+
+        [SerializeField,
+         Header("Минимальное расстояние до точки, после преодоления которого бот останавливается и бьет врага"),
+         Range(0f, 1f)]
+        private float _minDistanceToPoint;
+
+        [SerializeField, Header("Название параметра в Аниматоре, которй переключает анимации")]
+        private string _nameAnimatorSwitcher;
+
+        [SerializeField, Header("Значение при котором у врага будет проигрываться анимация idle"), Range(0f, 1f)]
+        private float _numberIdleAnimator;
+
+        [SerializeField, Header("Значение при котором у врага будет проигрываться анимация движения"), Range(0f, 1f)]
+        private float _numberWalkAnimator;
+
+        [SerializeField, Header("Значение при котором у врага будет проигрываться анимация атаки"), Range(0f, 1f)]
+        private float _numberAttackAnimator;
+    }
+}
