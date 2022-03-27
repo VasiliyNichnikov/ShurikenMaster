b61af8be32eb82ec5f92c4b16c9b4084f6862274
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-03-27 23:59:26 +0300

    Добавлена сборка, которая отвечает за разрезание объектов

---
+++ Assets/Scripts/CuttingWithShurikens/TestSlice.cs
@@ -0,0 +1,29 @@
+﻿using SliceFramework;
+using UnityEngine;
+
+namespace CuttingWithShurikens
+{
+    public class TestSlice : MonoBehaviour
+    {
+        [SerializeField] private GameObject _objectToSlice;
+        [SerializeField] private Material _material;
+
+        private void Start()
+        {
+            SlicedHull hull = _objectToSlice.Slice(transform.position, transform.up, _material);
+            if (hull != null)
+            {
+                print("hull");
+                GameObject left = hull.CreateLowerHull(_objectToSlice, _material);
+                GameObject right = hull.CreateUpperHull(_objectToSlice, _material);
+                
+                // left.SetRotationY(objectToSlice.transform.eulerAngles.y);
+                // right.SetRotationY(objectToSlice.transform.eulerAngles.y);
+                //
+                // slicedObjectLeftPos.SetPositionY(left.GetPositionY());
+                // slicedObjectRightPos.SetPositionY(right.GetPositionY());
+            }
+        }
+        
+    }
+}
